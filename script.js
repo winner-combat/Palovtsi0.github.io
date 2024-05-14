4b17f19c8b7db103a31c3a0f8cc9e08ab3b6c36a
winner-combat <147323876+winner-combat@users.noreply.github.com>
2024-05-14 22:15:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("modal");
+    var registerLink = document.getElementById("registerLink");
+    var closeBtn = document.getElementsByClassName("close")[0];
+
+    registerLink.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+// Функція для прокрутки вгору
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Показати або приховати кнопку прокрутки вгору при необхідності
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+});
